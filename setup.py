7b9aaf6e5e68588922a2fff540663eb392820240
Brandon Quinne <brandon@luckeyhomes.com>
2019-10-19 19:57:19 +0200

    update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="apipkg",
-    version="0.0.6",
+    version="0.0.7",
     author="Socle technique SIGL 2020",
     author_email="brandonquinne@hotmail.fr",
     description="Package for api manager kong service wrapper ",
